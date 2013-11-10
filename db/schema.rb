b5f2ffcccd6ec7396b553cbed197288cb6878286
Harrison Powers <fijimunkii@gmail.com>
2013-11-10 01:56:59 -0500

    add location to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131110065324) do
+ActiveRecord::Schema.define(:version => 20131110065626) do
 
   create_table "users", :force => true do |t|
     t.string   "provider"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20131110065324) do
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
     t.string   "image"
+    t.string   "location"
   end
 
 end
