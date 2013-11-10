6fe8e1ce707cf98ccc594f11b5c201d4e9eb5543
Harrison Powers <fijimunkii@gmail.com>
2013-11-10 01:46:38 -0500

    proper omniauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Hintr::Application.routes.draw do
 
   root to: 'welcome#index'
 
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+
   resources :users do
     resources :likes
     resources :hints do
