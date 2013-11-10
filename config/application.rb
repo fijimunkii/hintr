3f535a75aeb2086e7bd6603074e251aec3612f08
Harrison Powers <fijimunkii@gmail.com>
2013-11-10 01:25:27 -0500

    disable precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Hintr
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Needed for pushing to heroku
+    config.assets.initialize_on_precompile = false
   end
 end
