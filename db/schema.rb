5f5c9119919f276099473d66dd6c2997e3378d4e
Harrison Powers <fijimunkii@gmail.com>
2013-11-10 01:55:03 -0500

    add image column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131110065109) do
+ActiveRecord::Schema.define(:version => 20131110065324) do
 
   create_table "users", :force => true do |t|
     t.string   "provider"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20131110065109) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "image"
   end
 
 end
