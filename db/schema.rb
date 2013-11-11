1c0fa8a44dc229fb1c0cd2c3d4733d8a16de5531
Harrison Powers <fijimunkii@gmail.com>
2013-11-11 10:57:04 -0500

    matches table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131111155357) do
+ActiveRecord::Schema.define(:version => 20131111155549) do
 
   create_table "hints", :force => true do |t|
     t.integer  "user_id"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(:version => 20131111155357) do
   create_table "likes", :force => true do |t|
     t.integer  "fb_id"
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "matches", :force => true do |t|
+    t.integer  "like_id"
+    t.integer  "user_id"
+    t.integer  "hint_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
