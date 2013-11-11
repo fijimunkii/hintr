25bd21271cd4aef56335ee7bbba1dac55bc21661
Harrison Powers <fijimunkii@gmail.com>
2013-11-11 10:55:28 -0500

    likes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131111154425) do
+ActiveRecord::Schema.define(:version => 20131111155357) do
 
   create_table "hints", :force => true do |t|
     t.integer  "user_id"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(:version => 20131111154425) do
     t.integer  "score"
     t.datetime "created_at",                       :null => false
     t.datetime "updated_at",                       :null => false
+  end
+
+  create_table "likes", :force => true do |t|
+    t.integer  "fb_id"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
