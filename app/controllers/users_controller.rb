d941c7d23e72283ce0a51cdaaef8d4db5cb16bbc
Harrison Powers <fijimunkii@gmail.com>
2013-11-11 09:10:21 -0500

    getting started on monday

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def oauth_failure
+    # on OmniAuth error, create a flash error and redirect to the homepage
     flash[:error] = 'There was an issue connecting to facebook..'
     redirect_to :root
   end
