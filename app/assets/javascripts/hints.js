48f49a1c41516821c41ed4d780dadeda08b5ce16
Will Smith <will.smith@preplaysports.com>
2013-11-12 12:04:54 -0500

    match styling

--- app/assets/javascripts/hints.js
+++ app/assets/javascripts/hints.js
@@ -1,15 +1,15 @@
-// function Hints() {
-//   this.generateHints(44, 100, 12);
-// }
 var createHints = function() {
     showHints().done(function(data) {
       for (i=0;i<data.length;i++) {
         $matchDiv = $('<div>');
         $matchDiv.addClass('match');
+        $matchDiv.addClass('col-md-3 col-lg-3');
         $matchPic = $('<img>');
         $matchPic.attr('src', data[i]['profile_picture']);
+        $matchPic.attr('class', 'img-circle');
         $matchDiv.append($matchPic);
-        $('.row').append($matchDiv);
+        $('#hint-rows').append($matchDiv);
+        $('#show-hints').addClass('hidden');
       }
     });
     $(this).off();
