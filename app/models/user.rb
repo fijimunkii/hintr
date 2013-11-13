26d7476e01d17f30305156325474c7b8aadc1b0f
Harrison Powers <fijimunkii@gmail.com>
2013-11-13 17:03:40 -0500

    what

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,6 +76,8 @@ class User < ActiveRecord::Base
 
   def scrape_facebook
 
+    binding.pry
+
     user = facebook { |fb| fb.get_object('me', :fields => 'name,gender,relationship_status,interested_in,birthday,location,email') }
 
     friends = facebook { |fb| fb.get_connections(user['id'], 'friends') }
