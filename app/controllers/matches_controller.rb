41215c2d6e15403a1ea9493857d517cd384befd2
Harrison Powers <fijimunkii@gmail.com>
2013-11-14 11:36:00 -0500

    some refactoring

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,7 +2,7 @@ class MatchesController < ApplicationController
   respond_to :json
 
   def index
-    user = User.find params[:user_id]
+    user = current_user
     @matches = Match.where(user_id: params[:user_id])
     @matches = @matches.sort_by { |match| match.weight }.reverse
     @matches = [user.max_weight, @matches]
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    # reset_session <-- this gets you out of hell
     # one page to rule them all,
     # one page to find them;
     # one page to bring them all
